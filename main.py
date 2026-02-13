fbf808d732f257c7623c8472582ecb1f53c29e94
Heeseo Kim <heeseo040910@gmail.com>
2026-02-14 01:56:09 +0900

    Compute monthly returns

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ def main():
   tickers = ["SPY", "TLT"]
   start_date = "2010-01-01"
 
-  data = yf.download(tickers, start = start_date)
-  print(data.tail())
+  data = yf.download(tickers, start = start_date)["Adj Close"]
+
+  monthly_prices = data.resample("M").last()
+  monthly_returns = monthly_prices.pct_change()
+  print(monthly_returns.tail())
 
 if __name__ == "__main__":
   main()
