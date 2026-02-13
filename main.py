b7e29ffb3a00dfe94ca1b534ccdd00081664e080
Heeseo Kim <heeseo040910@gmail.com>
2026-02-14 01:38:42 +0900

    Download SPY and TLT data

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+
+import yfinance as yf
+
 def main(): 
   print("quant-momentum-backtest initialized") 
+  tickers = ["SPY", "TLT"]
+  start_date = "2010-01-01"
+
+  data = yf.download(tickers, start = start_date)
+  print(data.tail())
 
 if __name__ == "__main__":
   main()
